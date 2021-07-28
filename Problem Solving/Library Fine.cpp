d7e11ed1ebb0e4117071707af5a1a0dfb8606abf
shivamgt <66057250+shivamgt@users.noreply.github.com>
2021-07-28 17:48:51 +0530

    Create Library Fine.cpp

---
+++ Problem Solving/Library Fine.cpp
@@ -0,0 +1,11 @@
+int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) 
+{    
+    if(d1>d2 && m1==m2 && y1==y2)
+        return 15*(d1-d2);
+    else if(m1>m2 && y1==y2)
+        return 500*(m1-m2);
+    else if(y1>y2)
+        return 10000;
+    else 
+        return 0;            
+}
